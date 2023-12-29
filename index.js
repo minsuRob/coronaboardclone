8103bbae8b34dca36ea6a08dfa59192b9eb62bf8
minsuRob <minsu.rob@gmail.com>
2023-12-29 23:11:45 +0900

    fix: 호출 함수 객체 확인

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./database');
+const { getAll, insertOrUpdate, remove } = require('./controller/global-stat.controller');
 
 async function launchServer() {
     const app = express();
 
     app.use(bodyParser.json());
 
-    app.get('', (req, res)=> {
-        res.json(({message : "hello cororong"}));
-    });
+    app.get('/global-stats', getAll);
+    app.post('/global-stats', insertOrUpdate);
+    app.delete('/global-stats', remove);
 
     try {
         await sequelize.sync();
