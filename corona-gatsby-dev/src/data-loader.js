70b2f65607b771bdb48f0de8a5fe8acb0d52b394
minsuRob <minsu.rob@gmail.com>
2024-03-12 01:08:47 +0900

    209p - axio 호출부 추가

--- corona-gatsby-dev/src/data-loader.js
+++ corona-gatsby-dev/src/data-loader.js
@@ -1,14 +1,49 @@
+const axios = require('axios');
+const { subDays } = require('data-fns');
+const { format, utcToZonedTime} = require('date-fns-tz');
+
 const _ = require("loadash");
 const countInfo = require('../../tools/downloaded/countryInfo.json');
 
 
 async function getDataSource() {
     const countryByCc = _.keyBy(countInfo, 'cc');
+    const globalStats = await generateGlobalStats();
     
     return {
+        globalStats,
         countryByCc,
     };
 }
+
+async function generateGlobalStats() {
+    const apiClient = axios.create({
+        baseURL: process.env.CORONABOARD_API_BASE_URL || 'http://localhost:8080',
+    });
+
+    const response = await apiClient.get('global-stas');
+    const groupByDate = _.groupBy(response.data.result, 'date');
+
+    const now = new Date('2021-06-05');
+    const timeZone = 'Asia/Seoul';
+    const today = format(utcToZonedTime(now, timeZone), 'yyyy-MM-dd');
+    const yesterday = format(
+      utcToZonedTime(subDays(now, 1), timeZone),
+      'yyyy-MM-dd',
+    );
+
+    if (!groupByDate[today]) {
+        throw new Error("data for today is missing");
+    }
+
+    return createGlobalStatWithPrevField(
+        groupByDate[today],
+        groupByDate[yesterday],
+    )
+
+}
+
+    
 
 module.exports = {
     getDataSource,
