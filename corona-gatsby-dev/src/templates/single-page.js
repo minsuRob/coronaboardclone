6b847714e9052de33e6f7135e5def9c4acbc8997
minsuRob <minsu.rob@gmail.com>
2024-03-18 12:14:49 +0900

    213p - data source 추출 및 데이터 정제 코드 추가

--- corona-gatsby-dev/src/templates/single-page.js
+++ corona-gatsby-dev/src/templates/single-page.js
@@ -1,19 +1,86 @@
 import React from 'react';
 import { Slide } from '../components/slide';
+import { getDataSource } from '../data-loader';
+const axios = require('axios');
+const _ = require("lodash");
+
+const { subDays } = require('date-fns');
+const { format, utcToZonedTime} = require('date-fns-tz');
+
+
+async function generateGlobalStats() {
+    const apiClient = axios.create({
+        baseURL:'http://localhost:8080/',
+    });
+
+    const response = await apiClient.get('global-stats');
+    const groupByDate = _.groupBy(response.data.result, 'date'); 
+    
+    const now = new Date('2020-01-22');
+    const timeZone = 'Asia/Seoul';
+    const today = format(utcToZonedTime(now, timeZone), 'yyyy-MM-dd');
+    const yesterday = format(
+      utcToZonedTime(subDays(now, 1), timeZone),
+      'yyyy-MM-dd',
+    );
+
+    if (!groupByDate[today]) {
+        throw new Error("data for today is missing");
+    }
+
+    return createGlobalStatWithPrevField(
+        groupByDate[today],
+        groupByDate[yesterday],
+    )
+
+}
+
+function createGlobalStatWithPrevField(todayStats, yesterdayStats) {
+    const yesterdayStatsByCc = _.keyBy(yesterdayStats, 'cc');
+  
+    const globalStatWithPrev = todayStats.map((todayStat) => {
+      const cc = todayStat.cc;
+      const yesterdayStat = yesterdayStatsByCc[cc];
+      if (yesterdayStat) {
+        return {
+          ...todayStat,
+          confirmedPrev: yesterdayStat.confirmed || 0,
+          deathPrev: yesterdayStat.death || 0,
+          negativePrev: yesterdayStat.negative || 0,
+          releasedPrev: yesterdayStat.released || 0,
+          testedPrev: yesterdayStat.tested || 0,
+        };
+      }
+  
+      return todayStat;
+    });
+  
+    return globalStatWithPrev;
+  }
+
+  async function generateGlobalStats2() {
+    // const dataSource = await getDataSource();
+    // console.log(dataSource);
+  }
 
 export default function SinglePage({ pageContext }) {
     const {dataSource} = pageContext;
-    // const {thirdSlideTitle} = dataSource;
-    // const {countryByCc, globalStats} = dataSource;
+    // generateGlobalStats2();
+    // console.log(generateGlobalStats());
+    //   const groupByDate = _.groupBy(response.data.result, 'date'); 
+    
 
-    // console.log(countryByCc);
-    // console.log(globalStats);
+    // const {thirdSlideTitle} = dataSource;
+    const {countryByCc, globalStats} = dataSource;
+    console.log(countryByCc);
+    console.log(globalStats);
+
     return (
         <div>
             <h1>야로나!!보드</h1>
             <p>made by createPage</p>
             <Slide title="국가 별 상황"> 국가별 현황</Slide>
-  <Slide title={"국가 별 상황"}> 국가별 현황1 오호</Slide>
+      <Slide title={"국가 별 상황"}> 국가별 현황1 오호1</Slide>
     {/* <Slide title={thirdSlideTitle}> 국가별 현황</Slide> */}
         </div>
     )
